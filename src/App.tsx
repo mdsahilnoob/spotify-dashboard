a7b0948dd6b98ec63395e9ab6293af6da4b98cd1
MD SAHIL <144834170+mdsahilnoob@users.noreply.github.com>
2026-01-31 22:40:27 +0530

    setup shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './index.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
